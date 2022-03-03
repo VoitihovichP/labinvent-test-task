76da5bf8c24e0e2fec847c07d8527e5cc787346f
Pavel Voitihovich <pasha2003228@gmail.com>
2022-03-03 17:15:14 +0300

    feat: add close button for widgetsbar

--- labinvent-test/src/components/Widgetbar/Widgetsbar.tsx
+++ labinvent-test/src/components/Widgetbar/Widgetsbar.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { PageText } from '../../constants/enums';
 import WidgetsCard from '../WidgetsCard/WidgetsCard';
 import './widgetsbar.scss';
+import ArrowIcon from '../../assets/svg/arrow-icon.svg';
 
 const Widgetsbar: React.FC = () => {
   return (
     <div className="widgets">
       <WidgetsCard cardTitle={PageText.WIDGETS_CARD_SAMPLER} />
       <WidgetsCard cardTitle={PageText.WIDGETS_CARD_PUMP} />
+      <button className="widgets__close-btn">
+        <svg className="widgets__close-btn-icon">
+          <use href={`${ArrowIcon}#arrow`}></use>
+        </svg>
+      </button>
     </div>
   )
 }
